5a411f1486368122180668991d57867d59f3ff43
Diego Garcia <d.garci13@hotmail.com>
2020-05-04 21:43:03 -0600

    added full validation'

--- PICK_1.0/src/UI/LogValidation.py
+++ PICK_1.0/src/UI/LogValidation.py
@@ -2,65 +2,105 @@ import os
 import time
 import datetime
 import calendar
-from Models.EAR import EAR
+import pandas as pd
+import re
 
 
 def check_time(Logdate, start, end):
     log_date = Logdate.split()
-    m = list(calendar.month_abbr).index(log_date[1])
 
-    sd = datetime.date(int(start[2]), int(start[0]), int(start[1]))
-    ed = datetime.date(int(end[2]), int(end[0]), int(end[1]))
-    lg = datetime.date(int(log_date[4]), int(m), int(log_date[2]))
+    st = datetime.date(int(start[2]), int(start[0]), int(start[1]))
+    en = datetime.date(int(end[2]), int(end[0]), int(end[1]))
+    lg = datetime.date(int(log_date[2]), int(log_date[0]), int(log_date[1]))
 
-    if(sd>lg):
-        #log file date is before start-date
+    if (st > lg):
+        # log file date is before start-date
         return -1
-    elif(ed<lg):
-        #log file date is after end-date
+    elif (en < lg):
+        # log file date is after end-date
         return -1
     else:
         return 1
 
-def validate_log(file, startDate , endDate):
-    earList = []
+
+def validate_log(file, startDate, endDate):
     logPath = os.path.abspath(str(file))
     print("Log: %s" % logPath)
     dir = os.path.dirname(logPath)
-    log_date = time.ctime(os.path.getctime(logPath))
-    print(log_date)
     sd = startDate.split("/")
     ed = endDate.split("/")
 
-    dir = dir.lower().endswith(('white'))
-    fileType = logPath.endswith('.csv')
+    dir = dir.lower().endswith(('test_dir'))
+    txt_Type = logPath.endswith('.txt')
+    csvType = logPath.endswith('.csv')
+    excelType = logPath.endswith('.xlsx')
 
-    if(dir and fileType):
+    if (txt_Type):
+        valid = txt_logs(file, sd, ed)
+    elif (dir and csvType):
         print("csv and white dir")
         sd[1] = int(sd[1]) - 1
         ed[1] = int(ed[1]) + 1
-        valid = check_time(log_date, sd, ed)
+        valid = csv_logs(file, sd, ed)
+
+    elif (excelType):
+        valid = excel_logs(file, sd, ed)
+
+    elif (csvType):
+        valid = csv_logs(file, sd, ed)
+
     else:
-        valid = check_time(log_date, sd, ed)
-    if(valid == 1):
-        #proceed with splunk ingestion
+        valid = check_time(file, sd, ed)
+
+    if (valid == 1):
+        # proceed with splunk ingestion
         print("valid, proceed")
         return 1
     else:
-        #enforcment action
-        obj = EAR(logPath, log_date, "", "", "Timestamp out of bounds")
-        earList.append(obj)
+        # enforcment action
         print("Timestamp out of bounds")
         return -1
 
 
+def txt_logs(file, startDate, endDate):
+    f = open(file, "r")
+    content = f.read()
+    pattern = "\d{2}[:]\d{2}\s\d{2}[/]\d{2}[/]\d{2}\s\w\w"
+    dates = re.findall(pattern, content)
+    sd = startDate.split("/")
+    ed = endDate.split("/")
+
+    for x in dates:
+        p = x.split(" ")
+        d = p[1].split("/")
+        logDate = d[0] + " " + d[1] + " " + d[2]
+        print("Timestamp: " + x)
+        v = check_time(logDate, sd, ed)
+    return v
 
 
+def excel_logs(file, startDate, endDate):
+    s = pd.read_excel(file).to_string()
+    pattern = "\d{2}[:]\d{2}\s\d{2}[/]\d{2}[/]\d{2}\s\w\w"
+    dates = re.findall(pattern, s)
+    for x in dates:
+        p = x.split(" ")
+        d = p[1].split("/")
+        logDate = d[0] + " " + d[1] + " " + d[2]
+        print("Timestamp: " + x)
+        v = check_time(logDate, startDate, endDate)
+    return v
 
-"""
-if __name__ == '__main__':
-    s = "02/26/2020"
-    e = "03/26/2021"
-    file = "sample.txt"
-    validate_log(file ,s, e)
-"""
+
+def csv_logs(file, startDate, endDate):
+    err = []
+    s = pd.read_csv(file).to_string()
+    pattern = "\d{2}[:]\d{2}\s\d{2}[/]\d{2}[/]\d{2}\s\w\w"
+    dates = re.findall(pattern, s)
+    for x in dates:
+        p = x.split(" ")
+        d = p[1].split("/")
+        logDate = d[0] + " " + d[1] + " " + d[2]
+        print("Timestamp: " + x)
+        v = check_time(logDate, startDate, endDate)
+    return v
